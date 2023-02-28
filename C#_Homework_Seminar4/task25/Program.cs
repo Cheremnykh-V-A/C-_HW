f596f56d89ac8fa3d285d67f418be778059bfc28
Vadim Cheremnykh <chere96@yandex.ru>
2023-02-28 11:56:21 +0400

    add all folders

---
+++ C#_Homework_Seminar4/task25/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) 
+// и возводит число А в натуральную степень В
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+int ReadNumber(string messageToUser)
+{
+    Console.WriteLine(messageToUser);
+   int number = Convert.ToInt32(Console.ReadLine()); 
+    return number;
+}
+
+int numA = ReadNumber("Введите число А");
+int numB = ReadNumber("Введите число В");
+
+int numC = 1;
+
+if (numB >= 0)
+{
+    for (int count = 0; count < numB; count++)  
+    {
+        numC *= numA;
+    }
+    Console.WriteLine($"Число {numA} в степени {numB} равняется {numC}");
+}
+else
+{
+    Console.WriteLine("Число В не может быть меньше 0");
+}
---
+++ C#_Homework_Seminar4/task27/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая принимает на вход число 
+// и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int ReadNumber(string messageToUser)
+{
+    Console.WriteLine(messageToUser);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int number = ReadNumber("Введите число");
+
+int result = 0;
+while (number > 0)
+{
+    result += number % 10;
+    number = number / 10;
+}
+Console.WriteLine($"Сумма всех цифр в данном числе равна {result}");
+
+
+
+
