771c5691e38f83b6f7a48d22a0d98fa557bc5bf0
Vadim Cheremnykh <chere96@yandex.ru>
2023-03-04 12:02:17 +0400

    added task47

---
+++ C#_Homework_Seminar7/task47/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, 
+// заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+int ReadNumber(string messageToUser)
+{
+    Console.WriteLine(messageToUser);
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+
+int rows = ReadNumber("Введите число строк");
+int colums = ReadNumber("Введите число столбцов");
+
+double[,] matrix = new double[rows, colums];
+
+void GetRandomMatrix (double[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = Convert.ToDouble(Random.Shared.Next(-100, 100)) / 10;
+        }
+    }
+}
+
+void PrintMatrix (double[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+GetRandomMatrix(matrix);
+PrintMatrix(matrix);
